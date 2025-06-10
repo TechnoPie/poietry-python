72c2950148c05946b8118ec90ee1e9683909e707
Abhishek Mishra <34658093+TechnoPie@users.noreply.github.com>
2025-06-10 18:33:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,9 @@
 [tool.poetry]
 name = "my-python-project"
 version = "0.1.0"
-description = ""
-authors = ["Your Name <you@example.com>"]
-readme = "README.md"
-packages = [{include = "my_python_project"}]
 
 [tool.poetry.dependencies]
-python = "^3.8" # Or whatever Python version Poetry detected/you specified
+python = "^3.8"
 numpy = [
     { version = "1.17.3", markers = "python_version < '3.9'" },
     { version = "1.21.1", markers = "python_version >= '3.9'" },
